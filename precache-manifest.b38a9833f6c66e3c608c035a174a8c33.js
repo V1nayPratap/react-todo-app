8f713d1d65ca5b4b41fc53d2aafb211d30bd5f5a
Vinay Pratap <pratap221098@gmail.com>
2020-05-31 20:02:22 +0530

    Updates

---
+++ precache-manifest.b38a9833f6c66e3c608c035a174a8c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c42ff4a761556150762b584ec782fac",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "315186560299a014d546",
+    "url": "/react-todo-app/static/css/main.676b8545.chunk.css"
+  },
+  {
+    "revision": "86b33849b14934c46a6c",
+    "url": "/react-todo-app/static/js/2.a9b9974c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.a9b9974c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315186560299a014d546",
+    "url": "/react-todo-app/static/js/main.321767f0.chunk.js"
+  },
+  {
+    "revision": "3ce0fb85b9f2e40cc358",
+    "url": "/react-todo-app/static/js/runtime-main.7577f291.js"
+  }
+]);
